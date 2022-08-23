74a3e5f0e8b8b4c8e466c16ad9bcb509b8befca8
Kotmin <ptlusty@proton.me>
2022-08-24 00:16:23 +0200

    Lots of questions

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import sys, os
+
+# sys.path.append(os.path.join(os.path.dirname(__file__),'.','res'))
+
+# import res
+
+# print(constants.API_KEY_WEATHER)
+d = os.getcwd()
+
+print(f'Current: {d}') # curr directory
+
+# Python 3 Episode 22 Navigationg folders
+# Importing from a realtive path in Python
